edefca2f01a13fe98f270adbbae861862a795997
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2024-04-02 18:42:01 +0500

    change the title name and remove the logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,9 +9,9 @@ class MyDocument extends Document {
             name="description"
             content="Solana tools to help you in the solana ecosystem"
           />
-          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="icon" href="/favicon.ico" /> */}
 
-          <link
+          {/* <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/apple-touch-icon.png"
@@ -27,8 +27,8 @@ class MyDocument extends Document {
             type="image/png"
             sizes="16x16"
             href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
+          /> */}
+          {/* <link rel="manifest" href="/site.webmanifest" /> */}
           {/* <meta name="theme-color" content="#ffffff" /> */}
 
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
